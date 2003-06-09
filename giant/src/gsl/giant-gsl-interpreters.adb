3ebfbab15676a988c1f59e248e47e79a7345ce52
schulzgt <schulzgt>
2003-06-09 14:23:35 +0000

    first operations on Execution_Stack

--- giant/src/gsl/giant-gsl-interpreters.adb
+++ giant/src/gsl/giant-gsl-interpreters.adb
@@ -22,10 +22,12 @@
 --
 -- $RCSfile: giant-gsl-interpreters.adb,v $
 -- $Author: schulzgt $
--- $Date: 2003/06/06 20:02:22 $
+-- $Date: 2003/06/09 14:23:35 $
 --
 -- This package implements the datatypes used in GSL.
 --
+
+with Giant.Default_Logger;
 
 package body Giant.Gsl.Interpreters is
 
@@ -46,10 +48,17 @@ package body Giant.Gsl.Interpreters is
      (Individual : Interpreter;
       Name       : String) is
    begin
+      Default_Logger.Debug
+        ("Interpreter: Call compiler.", "Giant.Gsl");
       Individual.Execution_Stack :=
         Giant.Gsl.Compilers.Get_Execution_Stack
           (Individual.Gsl_Compiler, Name);
-      -- Individual.Result_Stack := Create
+      Default_Logger.Debug
+        ("Interpreter: Initilize result stack.", "Giant.Gsl");
+      Individual.Result_Stack := Result_Stacks.Create;
+      Default_Logger.Debug
+        ("Interpreter: Initilize Evolution Object.", "Giant.Gsl");
+      Initialize (Individual, 0);
    end Execute_Script;
 
    ---------------------------------------------------------------------------
@@ -57,8 +66,16 @@ package body Giant.Gsl.Interpreters is
    procedure Step
      (Individual  : access Interpreter_Record;
       Next_Action : out    Giant.Evolutions.Evolution_Action) is
+
+      Cmd : Syntax_Node;
    begin
-      Next_Action := Giant.Evolutions.Run; 
+      if Execution_Stacks.Is_Empty (Individual.Execution_Stack) then
+         Next_Action := Giant.Evolutions.Finish;
+      else
+         Execution_Stacks.Pop (Individual.Execution_Stack, Cmd);
+         Log_Syntax_Node (Cmd);
+         Next_Action := Giant.Evolutions.Run;
+      end if;
    end Step;
 
    ---------------------------------------------------------------------------
