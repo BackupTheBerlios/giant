d7894a98872fb5af0939c389269e55c58bb6cf88
koppor <koppor>
2003-06-10 00:30:41 +0000

    not working alpha-versions

---
+++ giant/src/vis/giant-layouts-tree_layouts.adb
@@ -0,0 +1,31 @@
+------------------------------------------------------------------------------
+--  GIANT - Graphical IML Analysis and Navigation Tool
+--
+--  Copyright (C) 2003 Philipp Haeuser, Steffen Keul, Oliver Kopp,
+--  Steffen Pingel, Gerrit Schulz and Martin Schwienbacher.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+--  First Author: Oliver Kopp
+--
+--  $RCSfile: giant-layouts-tree_layouts.adb,v $, $Revision: 1.1 $
+--  $Author: koppor $
+--  $Date: 2003/06/10 00:30:41 $
+--
+
+package body Giant.Layouts.TreeLayouts is
+
+end Giant.Layouts.TreeLayouts;
+
---
+++ giant/src/vis/giant-layouts.adb
@@ -0,0 +1,40 @@
+------------------------------------------------------------------------------
+--  GIANT - Graphical IML Analysis and Navigation Tool
+--
+--  Copyright (C) 2003 Philipp Haeuser, Steffen Keul, Oliver Kopp,
+--  Steffen Pingel, Gerrit Schulz and Martin Schwienbacher.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+--  First Author: Oliver Kopp
+--
+--  $RCSfile: giant-layouts.adb,v $, $Revision: 1.1 $
+--  $Author: koppor $
+--  $Date: 2003/06/10 00:30:41 $
+--
+
+package body Giant.Layouts is
+
+   function Create
+     return Create_Result is
+   begin
+      --  Initalize of the algorithm parses Addtional_Parameters and returns
+      --  result
+      --  Evoltuions.Initialize() is called from here
+      return Other_Error;
+   end Create;
+
+end Giant.Layouts;
+
