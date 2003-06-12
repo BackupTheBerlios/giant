3c614bfa5651a1202361512c3d2385051b404d60
koppor <koppor>
2003-06-12 13:59:43 +0000

    added Get_Graph_Hash

--- giant/src/graph_lib/giant-graph_lib.adb
+++ giant/src/graph_lib/giant-graph_lib.adb
@@ -18,9 +18,9 @@
 --  along with this program; if not, write to the Free Software
 --  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 --
---  $RCSfile: giant-graph_lib.adb,v $, $Revision: 1.9 $
+--  $RCSfile: giant-graph_lib.adb,v $, $Revision: 1.10 $
 --  $Author: koppor $
---  $Date: 2003/06/12 13:33:38 $
+--  $Date: 2003/06/12 13:59:43 $
 
 --  from ADA
 with Ada.Unchecked_Deallocation;
@@ -1488,6 +1488,12 @@ package body Giant.Graph_Lib is
    begin
       return Edge.Target_Node;
    end Get_Target_Node;
+
+   ---------------------------------------------------------------------------
+   function Get_Graph_Hash return Integer is
+   begin
+      return IML_Graphs.Hash (IML_Graph);
+   end Get_Graph_Hash;
 
    ---------------------------------------------------------------------------
    function Hash_Edge_Class_Id (Key : in Edge_Class_Id) return Integer is
