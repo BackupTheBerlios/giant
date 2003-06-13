265d66cc43045cea6a318edab506ff958fd404bb
koppor <koppor>
2003-06-13 17:59:02 +0000

    WIP

--- giant/test/giant_test-graph_lib.adb
+++ giant/test/giant_test-graph_lib.adb
@@ -20,21 +20,57 @@
 --
 --  First Author: Steffen Keul
 --
---  $RCSfile: giant_test-graph_lib.adb,v $, $Revision: 1.2 $
+--  $RCSfile: giant_test-graph_lib.adb,v $, $Revision: 1.3 $
 --  $Author: koppor $
---  $Date: 2003/06/12 14:24:42 $
+--  $Date: 2003/06/13 17:59:02 $
 --
 ------------------------------------------------------------------------------
 
 with Ada.Text_IO; use Ada.Text_IO;
 
-with Giant.Graph_Lib;
+with Giant; use Giant;
+
+with Giant.Graph_Lib; use Giant.Graph_Lib;
+
+with Giant.Default_Logger;
 
 procedure Giant_Test.Graph_Lib is
+
+   procedure Output_Attributes (Node : in Node_Id) is
+      Iter : Node_Attribute_Iterator;
+      Attr : Node_Attribute_Id;
+   begin
+      Iter := Make_Attribute_Iterator (Node);
+      while More (Iter) loop
+         Next (Iter, Attr);
+
+         Put_Line ("Attribute " & Convert_Node_Attribute_Id_To_Name (Attr));
+
+         case Get_Node_Attribute_Class_Id (Attr) is
+            when Class_Node_Id =>
+              null;
+            when Class_SLoc =>
+               null;
+            when others =>
+               null;
+         end case;
+
+      end loop;
+   end Output_Attributes;
+
 begin
-   Put_Line ("Bad thing - you'll se an exception NOW:");
+   Giant.Default_Logger.Init;
    Giant.Graph_Lib.Create ("/home/stsopra/giant/graphs/rfg_examp.iml");
+
+   -- q&d test of getting a single node
+   declare
+      Root : Node_Id;
+   begin
+      Root := Get_Root_Node;
+      Put ("Got Root Node with ID ");
+      Put_Line (Node_Id_Image (Root));
+   end;
+
    Giant.Graph_Lib.Destroy;
+   Giant.Default_Logger.Close;
 end Giant_Test.Graph_Lib;
-
-
