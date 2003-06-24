4575956892842838cff252645f2a1e0d2b2bcfcb
koppor <koppor>
2003-06-24 19:51:35 +0000

    only stubs

---
+++ giant/test/giant-graph_lib-node_attribute_filters-test.adb
@@ -0,0 +1,74 @@
+------------------------------------------------------------------------------
+--  GIANT - Graphical IML Analysis and Navigation Tool
+--
+--  Copyright (C) 2003 Philipp Haeuser, Steffen Keul, Oliver Kopp,
+--  Steffen Pingel, Gerrit Schulz and Martin Schwienbacher.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+--  First Author: Steffen Pingel
+--
+--  $RCSfile: giant-graph_lib-node_attribute_filters-test.adb,v $, $Revision: 1.1 $
+--  $Author: koppor $
+--  $Date: 2003/06/24 19:51:35 $
+--
+
+with Ada.Text_IO;
+
+with AUnit.Assertions; use AUnit.Assertions;
+with AUnit.Test_Cases.Registration; use AUnit.Test_Cases.Registration;
+
+with Giant.Graph_Lib.Node_Attribute_Filters;
+use Giant.Graph_Lib.Node_Attribute_Filters;
+with Giant.Logger;
+
+package body Giant.Graph_Lib.Node_Attribute_Filters.Test is
+
+   -------------------------------------
+   --  Global variables and routines  --
+   -------------------------------------
+
+   package Logger is new Giant.Logger("T:NAFilters");
+
+   -----------------
+   --  Testcases  --
+   -----------------
+
+   --------------------------------
+   --  Routines from AUnit-Test  --
+   --------------------------------
+
+   function Name (T : Test_Case) return Ada.Strings.Unbounded.String_Access is
+   begin
+      return new String'("Graph_Lib.Node_Attribute_Filters");
+   end Name;
+
+   procedure Register_Tests (T : in out Test_Case) is
+   begin
+      -- Register_Routine (T, Init'Access, "Init");
+      null;
+   end Register_Tests;
+
+   procedure Set_Up (T : in out Test_Case) is
+   begin
+      null;
+   end Set_Up;
+
+   procedure Tear_Down (T : in out Test_Case) is
+   begin
+      null;
+   end Tear_Down;
+
+end Giant.Graph_Lib.Node_Attribute_Filters.Test;
---
+++ giant/test/giant-graph_lib-node_attribute_filters-test.ads
@@ -0,0 +1,49 @@
+------------------------------------------------------------------------------
+--  GIANT - Graphical IML Analysis and Navigation Tool
+--
+--  Copyright (C) 2003 Philipp Haeuser, Steffen Keul, Oliver Kopp,
+--  Steffen Pingel, Gerrit Schulz and Martin Schwienbacher.
+--
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+--
+--  First Author: Steffen Pingel
+--
+--  $RCSfile: giant-graph_lib-node_attribute_filters-test.ads,v $, $Revision: 1.1 $
+--  $Author: koppor $
+--  $Date: 2003/06/24 19:51:46 $
+--
+------------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+with AUnit.Test_Cases;
+
+package Giant.Graph_Lib.Node_Attribute_Filters.Test is
+
+   type Test_Case is new AUnit.Test_Cases.Test_Case with null record;
+
+   --  Registers routines to be run.
+   procedure Register_Tests (T : in out Test_Case);
+
+   --  Provides name identifying the test case.
+   function Name (T : Test_Case) return Ada.Strings.Unbounded.String_Access;
+
+   --  Preparation performed before each routine.
+   procedure Set_Up (T : in out Test_Case);
+
+   --  Cleanup performed after each routine.
+   procedure Tear_Down (T :  in out Test_Case);
+
+end Giant.Graph_Lib.Node_Attribute_Filters.Test;
