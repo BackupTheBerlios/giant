2e9c629a654366fbef756997dd2987765c1f2861
koppor <koppor>
2003-07-03 01:15:32 +0000

    first version finished - should work fine for first tries

--- giant/src/vis/giant-matrix_layouts.adb
+++ giant/src/vis/giant-matrix_layouts.adb
@@ -20,10 +20,12 @@
 --
 --  First Author: Oliver Kopp
 --
---  $RCSfile: giant-matrix_layouts.adb,v $, $Revision: 1.3 $
+--  $RCSfile: giant-matrix_layouts.adb,v $, $Revision: 1.4 $
 --  $Author: koppor $
---  $Date: 2003/07/02 11:59:11 $
+--  $Date: 2003/07/03 01:15:32 $
 --
+
+with Ada.Numerics.Generic_Elementary_Functions;
 
 with Giant.Graph_Lib;
 
@@ -49,7 +51,9 @@ package body Giant.Matrix_Layouts is
       Res.State           := Init;
 
       --  Evolutions.Initialize
-      Initialize (Res);
+      Initialize
+        (Individual => Res,
+         Complexity => Graph_Lib.Node_Id_Sets.Size (Res.Nodes_To_Layout));
 
       return Res;
    end Initialize;
@@ -69,28 +73,127 @@ package body Giant.Matrix_Layouts is
       Next_Action :    out Evolutions.Evolution_Action)
    is
 
+      ----------------------------------------------------------------------
+      --  Removes top node of Layout.Nodes_To_Layout
+      --  and sets its position to given pos
       procedure Set_Position_Of_Top_Node
+        (Pos : in Vis.Logic.Vector_2d)
       is
+         Node : Graph_Lib.Node_Id;
       begin
-         null;
+         Graph_Lib.Node_Id_Sets.Remove_First
+           (Layout.Nodes_To_Layout, Node);
+         Graph_Widgets.Set_Top_Middle
+           (Layout.Widget,
+            Node,
+            Pos,
+            Layout.Widget_Lock);
       end Set_Position_Of_Top_Node;
+
+      procedure Init_Calculation
+      is
+        package Float_Functions is new
+          Ada.Numerics.Generic_Elementary_Functions (Float_Type => Float);
+      begin
+         Layout.Matrix_Width := Natural
+           (Float_Functions.Sqrt
+            (Float (Graph_Lib.Node_Id_Sets.Size
+                    (Layout.Nodes_To_Layout))));
+         Layout.Current_Column     := 1;
+         Layout.Current_Position   := Layout.Target_Position;
+         Layout.Current_Row_Height := 0.0;
+         Layout.Max_Node_Width     :=
+           Graph_Widgets.Get_Current_Maximum_Node_Width (Layout.Widget);
+      end Init_Calculation;
+
+      procedure Do_Calculation
+      is
+
+         function Min
+           (A : in Natural;
+            B : in Natural)
+           return Natural
+         is
+         begin
+            if A < B then
+               return A;
+            else
+               return B;
+            end if;
+         end Min;
+
+         function Max
+           (A : in Natural;
+            B : in Natural)
+           return Natural
+         is
+         begin
+            if A > B then
+               return A;
+            else
+               return B;
+            end if;
+         end Max;
+
+         Number_Nodes_To_Layout : Natural;
+
+      begin
+         Number_Nodes_To_Layout := Min
+           (Layout.Matrix_Width - Layout.Current_Column + 1,
+            Max_Nodes_In_One_Run);
+         Number_Nodes_To_Layout := Min
+           (Number_Nodes_To_Layout,
+            Graph_Lib.Node_Id_Sets.Size (Layout.Nodes_To_Layout));
+
+         for I in Layout.Current_Column..Number_Nodes_To_Layout loop
+            --  TBD: find next node by a breadth-first-search
+            --       (according to spec)
+            Set_Position_Of_Top_Node (Layout.Current_Position);
+
+            Layout.Current_Column := Layout.Current_Column + 1;
+            Vis.Logic.Set_X
+              (Layout.Current_Position,
+               Vis.Logic.Get_X (Layout.Current_Position) +
+               Layout.Max_Node_Width +
+               X_Distance);
+         end loop;
+
+         if Layout.Current_Column > Layout.Matrix_Width then
+            --  we have reached EOL, go to beginning of line
+            Layout.Current_Column := 1;
+            Layout.Current_Position :=
+              Vis.Logic.Combine_Vector
+              (Vis.Logic.Get_X (Layout.Target_Position),
+               Vis.Logic.Get_Y (Layout.Current_Position) +
+               Layout.Current_Row_Height);
+            Layout.Current_Row_Height := 0.0;
+         end if;
+
+         Evolutions.Advance_Progress (Layout, Number_Nodes_To_Layout);
+
+         if Graph_Lib.Node_Id_Sets.Size (Layout.Nodes_To_Layout) = 0 then
+            Next_Action := Evolutions.Finish;
+         else
+            Next_Action := Evolutions.Run;
+         end if;
+      end Do_Calculation;
 
    begin
       case Layout.State is
          when Init =>
-           if Graph_Lib.Node_Id_Sets.Size (Layout.Nodes_To_Layout) = 0 then
-              Next_Action := Evolutions.Finish;
-           elsif Graph_Lib.Node_Id_Sets.Size (Layout.Nodes_To_Layout) = 1 then
-              --  Set node-position to Target_Position
-              Next_Action := Evolutions.Finish;
-           else
-              --  real init
-              Layout.State := Calc;
-              Next_Action := Evolutions.Run;
-           end if;
-
+            case Graph_Lib.Node_Id_Sets.Size (Layout.Nodes_To_Layout) is
+               when 0 =>
+                  Next_Action := Evolutions.Finish;
+               when 1 =>
+                  Set_Position_Of_Top_Node (Layout.Target_Position);
+                  Next_Action := Evolutions.Finish;
+               when others =>
+                  Init_Calculation;
+                  Layout.State := Calc;
+                  Next_Action  := Evolutions.Run;
+            end case;
          when Calc =>
-            Next_Action := Evolutions.Finish;
+            Do_Calculation;
       end case;
    end Step;
 
