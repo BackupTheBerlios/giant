1397f534e2b15cf8d3f980bea26040629f236461
koppor <koppor>
2003-07-14 18:49:47 +0000

    fix at create

--- giant/src/graph_lib/giant-graph_lib-node_attribute_filters.adb
+++ giant/src/graph_lib/giant-graph_lib-node_attribute_filters.adb
@@ -20,14 +20,13 @@
 --
 --  First Author: Oliver Kopp
 --
---  $RCSfile: giant-graph_lib-node_attribute_filters.adb,v $, $Revision: 1.10 $
+--  $RCSfile: giant-graph_lib-node_attribute_filters.adb,v $, $Revision: 1.11 $
 --  $Author: koppor $
---  $Date: 2003/06/28 18:15:33 $
+--  $Date: 2003/07/14 18:49:47 $
 --
 ------------------------------------------------------------------------------
 
 with Ada.Strings.Unbounded;
-with Ada.Unchecked_Deallocation;
 
 package body Giant.Graph_Lib.Node_Attribute_Filters is
 
@@ -37,27 +36,49 @@ package body Giant.Graph_Lib.Node_Attribute_Filters is
       Node_Attribute_Names_List : in     String_Lists.List)
       return Filter
    is
+      Temp_Filter  : Filter;
       Res          : Filter;
       I            : Positive;
       Iter         : String_Lists.ListIter;
       Current_Name : Ada.Strings.Unbounded.Unbounded_String;
    begin
-      Res  := new Filter_Type
-        (1.. String_Lists.Length (Node_Attribute_Names_List));
+      Temp_Filter  := new Filter_Type
+        (1..String_Lists.Length (Node_Attribute_Names_List));
 
       Iter := String_Lists.MakeListIter (Node_Attribute_Names_List);
-      I    := 1;
+      I    := 0;
 
       --  Fill Filter with Node_Attributes
       while String_Lists.More (Iter) loop
          String_Lists.Next (Iter, Current_Name);
 
-         Res (I) :=
-           Convert_Node_Attribute_Name_To_Id
-           (Node_Class, Ada.Strings.Unbounded.To_String (Current_Name));
-
-         I:=I+1;
+         declare
+            Current_Name_As_String : String :=
+              Ada.Strings.Unbounded.To_String (Current_Name);
+         begin
+            if Graph_Lib.Does_Node_Attribute_Exist
+              (Node_Class          => Node_Class,
+               Node_Attribute_Name => Current_Name_As_String) then
+               --  Attribute exists in given Class, add it to the filter
+               I:=I+1;
+               Temp_Filter (I) :=
+                 Convert_Node_Attribute_Name_To_Id
+                 (Node_Class, Current_Name_As_String);
+            end if;
+         end;
       end loop;
+
+      --  Convert Temp_Filter to Res
+      --     by adjusting length
+      Res := new Filter_Type (1..I);
+
+      if Res'Length /= 0 then
+         --  "I" could also have been 0, if no given attribute existed in the
+         --     given Node_Class
+         Res (1..I) := Temp_Filter (1..I);
+      end if;
+
+      Free_Filter (Temp_Filter);
 
       return Res;
    end Create;
@@ -66,9 +87,6 @@ package body Giant.Graph_Lib.Node_Attribute_Filters is
    procedure Destroy
      (Node_Attribute_Filter  : in out Filter)
    is
-
-      procedure Free_Filter is new Ada.Unchecked_Deallocation
-        (Filter_Type, Filter);
 
    begin
       Free_Filter (Node_Attribute_Filter);
