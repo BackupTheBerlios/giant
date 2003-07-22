de50c4442af840e3be4fe02590144be5b6ac277e
koppor <koppor>
2003-07-22 09:48:52 +0000

    added dump of data

--- giant/test/dump_iml_data.adb
+++ giant/test/dump_iml_data.adb
@@ -20,21 +20,69 @@
 --
 --  First Author: Martin Schwienbacher
 --
---  $RCSfile: dump_iml_data.adb,v $, $Revision: 1.4 $
---  $Author: schwiemn $
---  $Date: 2003/07/10 12:27:21 $
---  
+--  $RCSfile: dump_iml_data.adb,v $, $Revision: 1.5 $
+--  $Author: koppor $
+--  $Date: 2003/07/22 09:48:52 $
+--
 -- -----
 -- Used to Dump the Content (Node Classes and attributes of an iml Graph)
 -- into the logger.
 --
-with Giant.Graph_Lib; 
-with Giant.Logger; 
+with Giant; use Giant;
+with Giant.Graph_Lib;
+with Giant.Graph_Lib.Subgraphs;
+with Giant.Logger;
 with Giant.Default_Logger;
 
-procedure dump_iml_data is 
+procedure dump_iml_data is
 
    package Logger is new Giant.Logger("dump_iml_data");
+
+   --------------------------------------------------------------------------
+   procedure Output_All_Nodes
+   is
+
+      procedure Execute (Node : in Graph_Lib.Node_Id)
+      is
+         Edges_In  : constant Graph_Lib.Edge_Id_Array :=
+           Graph_Lib.Get_Incoming_Edges (Node);
+         Edges_Out : constant Graph_Lib.Edge_Id_Array :=
+           Graph_Lib.Get_Outgoing_Edges (Node);
+      begin
+         Logger.Info ("Node " & Graph_Lib.Node_Id_Image (Node));
+
+         Logger.Info ("Edges in");
+         for I in Edges_In'Range loop
+            Logger.Info (Graph_Lib.Node_Id_Image
+                         (Graph_Lib.Get_Source_Node (Edges_In (I))));
+         end loop;
+
+         Logger.Info ("Edges out");
+         for I in Edges_Out'Range loop
+            Logger.Info (Graph_Lib.Node_Id_Image
+                         (Graph_Lib.Get_Target_Node (Edges_Out (I))));
+         end loop;
+      end Execute;
+
+      procedure Apply is new Graph_Lib.Node_Id_Sets.Apply
+        (Execute => Execute);
+
+      All_Nodes : Graph_Lib.Node_Id_Set;
+   begin
+      All_Nodes := Graph_Lib.Get_All_Nodes;
+
+      Apply (All_Nodes);
+
+      Graph_Lib.Node_Id_Sets.Destroy (All_Nodes);
+   end Output_All_Nodes;
+
+   procedure Load_And_Dump_Graph
+   is
+   begin
+      Graph_Lib.Load ("resources/rfg_examp.iml");
+      Output_All_Nodes;
+      Graph_Lib.Unload;
+   end Load_And_Dump_Graph;
 
    Node_Classes      : Giant.Graph_Lib.Node_Class_Id_Set;
    Node_Classes_Iter : Giant.Graph_Lib.Node_Class_Id_Sets.Iterator;
@@ -42,161 +90,164 @@ procedure dump_iml_data is
 
    Attributes_Iter : Giant.Graph_Lib.Node_Attribute_Iterator;
    A_Attribute     : Giant.Graph_Lib.Node_Attribute_Id;
-   
+
    Edge_Attr_Set  : Giant.Graph_Lib.Edge_Class_Id_Set;
    Edge_Attr_Iter : Giant.Graph_Lib.Edge_Class_Id_Sets.Iterator;
    A_Edge_Attr    : Giant.Graph_Lib.Edge_Class_Id;
-   
+
    Node_Class_Counter : Integer;
    Edge_Class_Counter : Integer;
    Attr_Counter       : Integer;
 
 begin
    Giant.Graph_Lib.Initialize;
-   
-   Giant.Default_Logger.Init ("a_iml_dump.log");
-   Logger.Info ("Starting Data Dump ...");
-         
-   Node_Classes := Giant.Graph_Lib.Get_All_Node_Class_Ids;   
 
-     
+   Giant.Default_Logger.Init ("an_iml_dump.log");
+   Logger.Info ("Starting Data Dump ...");
+
+   Node_Classes := Giant.Graph_Lib.Get_All_Node_Class_Ids;
+
+
    -- All node classes
    -------------------
    Logger.Info ("");
    Logger.Info ("");
    Logger.Info ("All Node Classes of the iml graph:");
    Logger.Info ("----------------------------------");
- 
-   Node_Classes_Iter := 
+
+   Node_Classes_Iter :=
      Giant.Graph_Lib.Node_Class_Id_Sets.Make_Iterator (Node_Classes);
-  
-   Node_Class_Counter := 1;   
+
+   Node_Class_Counter := 1;
    while Giant.Graph_Lib.Node_Class_Id_Sets.More (Node_Classes_Iter) loop
-  
-      Giant.Graph_Lib.Node_Class_Id_Sets.Next 
-        (Node_Classes_Iter, A_Node_Class);
-    
-      Logger.Info
-        (Integer'Image (Node_Class_Counter) & ". " & "Node Class: """
-         & Giant.Graph_Lib.Get_Node_Class_Tag (A_Node_Class)
-         & """"); 
-         
-      Node_Class_Counter := Node_Class_Counter + 1;
-   end loop; 
-   
-   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes_Iter); 
-   
-   
-   -- All edge classes
-   -------------------   
-   Logger.Info ("");
-   Logger.Info ("");
-   Logger.Info ("All Edge Classes of the iml graph:");
-   Logger.Info ("----------------------------------");
-   
-   Edge_Attr_Set := 
-     Giant.Graph_Lib.Get_All_Edge_Class_Ids;
-   
-   Edge_Attr_Iter := 
-     Giant.Graph_Lib.Edge_Class_Id_Sets.Make_Iterator (Edge_Attr_Set);
-     
-   Edge_Class_Counter := 1;
-   while Giant.Graph_Lib.Edge_Class_Id_Sets.More (Edge_Attr_Iter) loop
-                  
-      Giant.Graph_Lib.Edge_Class_Id_Sets.Next 
-        (Edge_Attr_Iter, A_Edge_Attr);
-                      
-      Logger.Info
-        (Integer'Image (Edge_Class_Counter) & ". " & "Edge Class: """              
-         & Giant.Graph_Lib.Get_Edge_Class_Tag 
-             (A_Edge_Attr)
-         & """");  
-         
-      Edge_Class_Counter := Edge_Class_Counter + 1;      
-   end loop;       
-    
-   Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Set);
-   Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Iter);   
-      
-      
-   -- Details for each node class
-   ------------------------------   
-   Logger.Info ("");
-   Logger.Info ("");  
-   Logger.Info ("Details about node classes of the iml graph:");
-   Logger.Info ("--------------------------------------------");
-   
-   Node_Classes_Iter := 
-     Giant.Graph_Lib.Node_Class_Id_Sets.Make_Iterator (Node_Classes);
-   
-   Node_Class_Counter := 1;  
-   while Giant.Graph_Lib.Node_Class_Id_Sets.More (Node_Classes_Iter) loop
-  
-      Giant.Graph_Lib.Node_Class_Id_Sets.Next 
+
+      Giant.Graph_Lib.Node_Class_Id_Sets.Next
         (Node_Classes_Iter, A_Node_Class);
 
-      Logger.Info ("");  
-      Logger.Info ("");      
       Logger.Info
         (Integer'Image (Node_Class_Counter) & ". " & "Node Class: """
          & Giant.Graph_Lib.Get_Node_Class_Tag (A_Node_Class)
          & """");
-      Logger.Info ("");      
-          
-            
+
+      Node_Class_Counter := Node_Class_Counter + 1;
+   end loop;
+
+   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes_Iter);
+
+
+   -- All edge classes
+   -------------------
+   Logger.Info ("");
+   Logger.Info ("");
+   Logger.Info ("All Edge Classes of the iml graph:");
+   Logger.Info ("----------------------------------");
+
+   Edge_Attr_Set :=
+     Giant.Graph_Lib.Get_All_Edge_Class_Ids;
+
+   Edge_Attr_Iter :=
+     Giant.Graph_Lib.Edge_Class_Id_Sets.Make_Iterator (Edge_Attr_Set);
+
+   Edge_Class_Counter := 1;
+   while Giant.Graph_Lib.Edge_Class_Id_Sets.More (Edge_Attr_Iter) loop
+
+      Giant.Graph_Lib.Edge_Class_Id_Sets.Next
+        (Edge_Attr_Iter, A_Edge_Attr);
+
+      Logger.Info
+        (Integer'Image (Edge_Class_Counter) & ". " & "Edge Class: """
+         & Giant.Graph_Lib.Get_Edge_Class_Tag
+             (A_Edge_Attr)
+         & """");
+
+      Edge_Class_Counter := Edge_Class_Counter + 1;
+   end loop;
+
+   Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Set);
+   Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Iter);
+
+
+   -- Details for each node class
+   ------------------------------
+   Logger.Info ("");
+   Logger.Info ("");
+   Logger.Info ("Details about node classes of the iml graph:");
+   Logger.Info ("--------------------------------------------");
+
+   Node_Classes_Iter :=
+     Giant.Graph_Lib.Node_Class_Id_Sets.Make_Iterator (Node_Classes);
+
+   Node_Class_Counter := 1;
+   while Giant.Graph_Lib.Node_Class_Id_Sets.More (Node_Classes_Iter) loop
+
+      Giant.Graph_Lib.Node_Class_Id_Sets.Next
+        (Node_Classes_Iter, A_Node_Class);
+
+      Logger.Info ("");
+      Logger.Info ("");
+      Logger.Info
+        (Integer'Image (Node_Class_Counter) & ". " & "Node Class: """
+         & Giant.Graph_Lib.Get_Node_Class_Tag (A_Node_Class)
+         & """");
+      Logger.Info ("");
+
+
       -- 1. Dump all attributes
       -------------------------
       Attributes_Iter := Giant.Graph_Lib.Make_Attribute_Iterator
         (A_Node_Class);
-                
+
       Attr_Counter := 1;
       while Giant.Graph_Lib.More (Attributes_Iter) loop
-      
+
          Giant.Graph_Lib.Next (Attributes_Iter, A_Attribute);
-         
+
          Logger.Info
-           ("  " & Integer'Image (Attr_Counter) & ". " & "Attribute: """                 
+           ("  " & Integer'Image (Attr_Counter) & ". " & "Attribute: """
             & Giant.Graph_Lib.Convert_Node_Attribute_Id_To_Name
                 (A_Attribute)
-            & """");     
-         Attr_Counter := Attr_Counter + 1; 
-            
+            & """");
+         Attr_Counter := Attr_Counter + 1;
+
       end loop;
-     
-      Logger.Info ("");  
-                   
-                   
+
+      Logger.Info ("");
+
+
       -- 2. Dump all edge attributes (subset of  1.)
-      ----------------------------------------------      
-      Edge_Attr_Set := 
+      ----------------------------------------------
+      Edge_Attr_Set :=
         Giant.Graph_Lib.Get_All_Edge_Class_Ids_For_Node_Class (A_Node_Class);
-   
-      Edge_Attr_Iter := 
+
+      Edge_Attr_Iter :=
         Giant.Graph_Lib.Edge_Class_Id_Sets.Make_Iterator (Edge_Attr_Set);
-        
+
       Edge_Class_Counter := 1;
       while Giant.Graph_Lib.Edge_Class_Id_Sets.More (Edge_Attr_Iter) loop
-      
-         Giant.Graph_Lib.Edge_Class_Id_Sets.Next 
+
+         Giant.Graph_Lib.Edge_Class_Id_Sets.Next
            (Edge_Attr_Iter, A_Edge_Attr);
-                      
+
          Logger.Info
-           ("  " & Integer'Image (Edge_Class_Counter) & ". " & "Edge_Attr: """                
-            & Giant.Graph_Lib.Get_Edge_Class_Tag 
+           ("  " & Integer'Image (Edge_Class_Counter) & ". " & "Edge_Attr: """
+            & Giant.Graph_Lib.Get_Edge_Class_Tag
                 (A_Edge_Attr)
             & """");
-            
-         Edge_Class_Counter := Edge_Class_Counter + 1;          
-      end loop;
-            
-      Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Set);
-      Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Iter);        
-      
-      Node_Class_Counter := Node_Class_Counter + 1;
-   end loop;      
 
-   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes_Iter);    
-   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes);   
+         Edge_Class_Counter := Edge_Class_Counter + 1;
+      end loop;
+
+      Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Set);
+      Giant.Graph_Lib.Edge_Class_Id_Sets.Destroy (Edge_Attr_Iter);
+
+      Node_Class_Counter := Node_Class_Counter + 1;
+   end loop;
+
+   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes_Iter);
+   Giant.Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Classes);
+
+   Load_And_Dump_Graph;
+
    Giant.Graph_Lib.Destroy;
 end dump_iml_data;
 
