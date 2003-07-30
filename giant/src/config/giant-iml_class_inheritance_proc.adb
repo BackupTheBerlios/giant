056c4e57f1ad3603ec3ca15c6d8481230fd6f88c
koppor <koppor>
2003-07-30 07:39:08 +0000

    changed to new graph_lib-interface

--- giant/src/config/giant-iml_class_inheritance_proc.adb
+++ giant/src/config/giant-iml_class_inheritance_proc.adb
@@ -20,9 +20,9 @@
 --
 --  First Author: Martin Schwienbacher
 --
---  $RCSfile: giant-iml_class_inheritance_proc.adb,v $, $Revision: 1.2 $
---  $Author: schwiemn $
---  $Date: 2003/07/14 19:15:00 $
+--  $RCSfile: giant-iml_class_inheritance_proc.adb,v $, $Revision: 1.3 $
+--  $Author: koppor $
+--  $Date: 2003/07/30 07:39:08 $
 --
 with DOM.Core.Elements;  -- from xmlada
 
@@ -35,67 +35,67 @@ with GIANT.Logger;  -- from GIANT
    ---------------------------------------------------------------------------
    --  <!ATTLIST super_node_class
    --    super_node_class_name CDATA #REQUIRED
-   --  >  
-   function Get_All_Sub_Node_Classes 
-     (XML_Node : in DOM.Core.Node) 
+   --  >
+   function Get_All_Sub_Node_Classes
+     (XML_Node : in DOM.Core.Node)
      return Graph_Lib.Node_Class_Id_Set is
-     
+
      New_Node_Class_Set : Graph_Lib.Node_Class_Id_Set;
      A_Node_Class_ID    : Graph_Lib.Node_Class_Id;
-     
+
    begin
-      
+
       A_Node_Class_ID := Graph_Lib.Convert_Node_Class_Name_To_Id
         (DOM.Core.Elements.Get_Attribute
-          (XML_Node, "super_node_class_name"));  
-      
-      New_Node_Class_Set := Graph_Lib.Get_Inherited_Classes
+          (XML_Node, "super_node_class_name"));
+
+      New_Node_Class_Set := Graph_Lib.Get_Successors
         (Node_Class     => A_Node_Class_ID,
          Include_Parent => True);
-                           
-      return New_Node_Class_Set;   
+
+      return New_Node_Class_Set;
    end Get_All_Sub_Node_Classes;
-     
-   ---------------------------------------------------------------------------   
+
+   ---------------------------------------------------------------------------
    --  <!ATTLIST super_edge_class
    --    super_start_node_class CDATA #REQUIRED
    --    attribute_name         CDATA #REQUIRED
-   --  >     
-   function Get_All_Sub_Edge_Classes 
-     (XML_Node : in DOM.Core.Node) 
+   --  >
+   function Get_All_Sub_Edge_Classes
+     (XML_Node : in DOM.Core.Node)
      return Graph_Lib.Edge_Class_Id_Set is
-   
+
       Node_Class_Set      : Graph_Lib.Node_Class_Id_Set;
       Node_Class_Set_Iter : Graph_Lib.Node_Class_Id_Sets.Iterator;
       A_Node_Class_ID     : Graph_Lib.Node_Class_Id;
       A_Node_Attribute_ID : Graph_Lib.Node_Attribute_Id;
-      
+
       A_Edge_Class_ID     : Graph_Lib.Edge_Class_id;
-      New_Edge_Class_Set  : Graph_Lib.Edge_Class_Id_Set;     
-      Temp_Edge_Class_Set : Graph_Lib.Edge_Class_Id_Set;      
+      New_Edge_Class_Set  : Graph_Lib.Edge_Class_Id_Set;
+      Temp_Edge_Class_Set : Graph_Lib.Edge_Class_Id_Set;
    begin
-      
+
       A_Node_Class_ID := Graph_Lib.Convert_Node_Class_Name_To_Id
         (DOM.Core.Elements.Get_Attribute
-          (XML_Node, "super_start_node_class"));  
-            
-      Node_Class_Set := Graph_Lib.Get_Inherited_Classes
+          (XML_Node, "super_start_node_class"));
+
+      Node_Class_Set := Graph_Lib.Get_Successors
         (Node_Class     => A_Node_Class_ID,
-         Include_Parent => True);     
+         Include_Parent => True);
       Node_Class_Set_Iter := Graph_Lib.Node_Class_Id_Sets.Make_Iterator
         (Node_Class_Set);
-                      
+
       New_Edge_Class_Set := Graph_Lib.Edge_Class_Id_Sets.Empty_Set;
-      
+
       while Graph_Lib.Node_Class_Id_Sets.More (Node_Class_Set_Iter) loop
-      
-         Graph_Lib.Node_Class_Id_Sets.Next 
+
+         Graph_Lib.Node_Class_Id_Sets.Next
            (Node_Class_Set_Iter, A_Node_Class_ID);
-           
+
          -- case 1 (node_class_name, edge_attribute_name)
          if (DOM.Core.Elements.Get_Attribute
            (XML_Node, "attribute_name") /= "*") then
-           
+
             A_Node_Attribute_ID := Graph_lib.Convert_Node_Attribute_Name_To_Id
               (Node_Class          => A_Node_Class_ID,
                Node_Attribute_Name => DOM.Core.Elements.Get_Attribute
@@ -112,24 +112,24 @@ with GIANT.Logger;  -- from GIANT
                Graph_Lib.Edge_Class_Id_Sets.Insert
                  (New_Edge_Class_Set, A_Edge_Class_ID);
             end if;
-         
+
          -- case 2 (node_class_name, "*")
          else
-         
-            Temp_Edge_Class_Set := 
-              Graph_Lib.Get_All_Edge_Class_Ids_For_Node_Class 
-                (A_Node_Class_ID);            
+
+            Temp_Edge_Class_Set :=
+              Graph_Lib.Get_All_Edge_Class_Ids_For_Node_Class
+                (A_Node_Class_ID);
             Graph_Lib.Edge_Class_Id_Sets.Union
               (New_Edge_Class_Set, Temp_Edge_Class_Set);
-            Graph_Lib.Edge_Class_Id_Sets.Destroy (Temp_Edge_Class_Set);              
+            Graph_Lib.Edge_Class_Id_Sets.Destroy (Temp_Edge_Class_Set);
          end if;
-                        
+
       end loop;
-            
-      Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Class_Set_Iter); 
-      Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Class_Set); 
-      
-      return New_Edge_Class_Set;          
+
+      Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Class_Set_Iter);
+      Graph_Lib.Node_Class_Id_Sets.Destroy (Node_Class_Set);
+
+      return New_Edge_Class_Set;
    end Get_All_Sub_Edge_Classes;
-        
+
 end Giant.IML_Class_Inheritance_Proc;
