71951345950772cbab6b88ff85f5dc21e7b33fc2
squig <squig>
2003-09-12 15:08:23 +0000

    fixed script

--- giant/giant.sh
+++ giant/giant.sh
@@ -5,8 +5,7 @@ system=`uname -s`
 if [ "$system" = "Linux" ]; then
 	./giant-Linux $*
 elif [ "$system" = "SunOS" ]; then
-	export LD_LIBRARY_PATH=src/iml_browser_030825:$LD_LIBRARY_PATH
-	./giant-SunOS $*
+	LD_LIBRARY_PATH=src/iml_browser_030825:$LD_LIBRARY_PATH ./giant-SunOS $*
 else
 	echo "$system is unsupported."
 	exit 1
