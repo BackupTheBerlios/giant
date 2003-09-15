7e0286f82980ced05cc416b23253cd0e5e0db506
schwiemn <schwiemn>
2003-09-15 19:34:04 +0000

    bug fixed in file_management - abs path was not claculated correctly

--- giant/test/giant-config-global_data-test.adb
+++ giant/test/giant-config-global_data-test.adb
@@ -20,9 +20,9 @@
 --
 --  First Author: Steffen Pingel
 --
---  $RCSfile: giant-config-global_data-test.adb,v $, $Revision: 1.8 $
+--  $RCSfile: giant-config-global_data-test.adb,v $, $Revision: 1.9 $
 --  $Author: schwiemn $
---  $Date: 2003/09/15 19:19:19 $
+--  $Date: 2003/09/15 19:34:04 $
 --
 
 with AUnit.Assertions; use AUnit.Assertions;
@@ -72,8 +72,8 @@ package body Giant.Config.Global_Data.Test is
            
       Logger.Debug ("Calculated_Path: "
         & File_Management.Get_Absolute_Path_To_File_From_Relative
-             ("./",
-              "./resources/config_glob_test/annotation_dir/document.xpm"));
+             ("./resources/config_glob_test/",
+              "./annotation_dir/document.xpm"));
       
       Logger.Debug ("--------------");
       Assert 
